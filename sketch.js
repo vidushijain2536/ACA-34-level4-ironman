6634ae0ecad1c40596e40a0aa9cc6ea8f62f7747
vidushijain2536 <71253509+vidushijain2536@users.noreply.github.com>
2021-03-27 21:04:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,121 @@
+
+var bg, backgroundImg;
+var ironman,man;
+var rockImg,rockGroup;
+var diamondGroup,diamondImg;
+var score=0;
+var spikeGroup,spikeImg
+
+function preload() {
+  backgroundImg = loadImage("images/bg.jpg");
+  ironman = loadImage("images/iron.png")
+  rockImg = loadImage("images/stone.png");
+  diamondImg= loadImage("images/diamond.png")
+  spikeImg =loadImage("images/spikes.png")
+
+}
+
+function setup() {
+  createCanvas(1000, 600);
+  bg = createSprite(580,300);
+  bg.addImage(backgroundImg)
+  man =createSprite(100,500)
+  man.addImage(ironman)
+  man.scale=0.3
+  ground = createSprite(200,585,2000,10);
+  ground.visible =false;
+  man.setCollider("rectangle",10,10,300,300);
+  rockGroup=new Group();
+  diamondGroup=new Group();
+  spikeGroup=new Group();
+  
+ 
+}
+
+function draw() {
+     if (keyDown("up")){
+     man.velocityY = -10;}
+    if (keyDown("left")){
+      man.x = man.x -7;
+    }
+    if (keyDown("right")){
+      man.x = man.x+7;}
+
+    
+  man.velocityY = man.velocityY + 0.5
+  
+  man.collide(ground)
+  bg.velocityY=-10;
+  if (bg.y<200){
+    bg.y=bg.width/4;}
+
+   
+    generateDiamonds();
+    for(var i = 0 ; i < (diamondGroup).length;i++){
+     temp=(diamondGroup).get(i);
+    if(temp.isTouching(man)){
+        score++;
+        temp.destroy();
+        temp=null;
+      }
+
+      
+    }
+    generatespike();
+    //for (var j = 0; j <spikekGroup.length; j++){
+      //var temp = spikeGroup.get(j);
+      //if(temp.isTouching(spike)){
+        spike.collide(temp);
+      }}
+    generaterock ();
+for (var i = 0; i <rockGroup.length; i++){
+      var temp = rockGroup.get(i);
+      if(temp.isTouching(man)){
+        man.collide(temp);
+      }
+    
+  }
+    
+    drawSprites();
+    textSize(20)
+   text("diamonds collected"+score,500,50);
+}
+function generaterock(){
+  if ( frameCount % 70 ===0){
+    var rock = createSprite(120,120,40,10);
+    rock.x=random(50,450);
+    rock.addImage(rockImg);
+    rock.scale =1;
+    rock.velocityY= 5;
+    rockGroup.add(rock);
+    
+  }
+
+}
+function generateDiamonds(){
+  if (frameCount % 50 === 0) {
+    var diamond = createSprite(1200,120,40,10);
+    diamond.addImage(diamondImg)
+    diamond.x = Math.round(random(80,400));
+    diamond.scale = 0.5;
+    diamond.velocityY= 3;
+    diamond.lifetime = 1200;
+    diamondGroup.add(diamond);
+  }
+
+}
+function generatespike(){
+if(frameCount % 60 === 0){
+  var spike = createSprite(1200,100,10,40);
+  spike.velocityY = +4;
+  spike.scale=2.5
+ spike.addImage(spikeImg);
+ spikeGroup.add(spike);
+ spike.lifetime=600;
+
+  }
+}
+
+
+
+
